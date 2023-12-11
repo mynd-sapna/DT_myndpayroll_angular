8cce1a09162c97042dbb0ec2e97f04f4679f0c70
sapna.kurmi <sapna.kurmi@MYNDLAP-4300>
2023-12-11 09:57:22 +0530

    all

--- DT_Payroll/src/auth/admin.guard.ts
+++ DT_Payroll/src/auth/admin.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthServiceService } from 'src/app/Services/auth-service.service';
 
@@ -7,7 +7,7 @@ import { AuthServiceService } from 'src/app/Services/auth-service.service';
   providedIn: 'root'
 })
 
-export class AdminGuard implements CanActivate {
+export class AdminGuard  {
     constructor(private authService: AuthServiceService, private router: Router) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
--- DT_Payroll/src/auth/auth.guard.ts
+++ DT_Payroll/src/auth/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthServiceService } from 'src/app/Services/auth-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private authService: AuthServiceService, private router: Router) {}
   canActivate(): any {
     let isLoggedIn = false;
