4009f167265951ca6cd19397b104c7c1cfcf2dfc
sapna.kurmi <sapna.kurmi@MYNDLAP-4300>
2023-12-18 13:42:56 +0530

    all

--- DT_Payroll/src/app/Material/material.module.ts
+++ DT_Payroll/src/app/Material/material.module.ts
@@ -42,7 +42,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
-import { MatTableDataSource } from '@angular/material/table';
+
 @NgModule({
   declarations: [],
   imports: [
@@ -129,9 +129,6 @@ import { MatTableDataSource } from '@angular/material/table';
     MatTreeModule,
     PortalModule,
     ScrollingModule,
-    MatTableModule,
   ],
 })
 export class MaterialModule { }
-
-export { MatProgressSpinnerModule };
